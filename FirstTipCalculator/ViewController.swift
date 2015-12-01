80f4926b3ddd8c5113caf6327f668d95212c5e24
Faisal <faisal.abujabal@gmail.com>
2015-12-01 01:05:49 -0600

    Initial Commit

---
+++ FirstTipCalculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FirstTipCalculator
+//
+//  Created by Faisal Abu Jabal on 12/1/15.
+//  Copyright © 2015 Faisal Abu Jabal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
